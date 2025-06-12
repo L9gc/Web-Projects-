f8abc660cc430dd544f0a176453eee5975e7b6c8
Luyanda Blose <161873288+L9gc@users.noreply.github.com>
2025-06-12 16:46:20 +0200

    Update Script.js

--- 10th Project - Dad Jokes/Script.js
+++ 10th Project - Dad Jokes/Script.js
@@ -19,18 +19,3 @@ async function generateJoke() {
 
          jokEL.innerHTML = data.joke
     }
-
-// Using.then() method
-// function generateJoke() {
-//     const config = {
-//         headers: {
-//             Accept: 'application/json'
-//         },
-//     }
-
-//     fetch('https://icanhazdadjoke.com/', config)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             jokEL.innerHTML = data.joke
-//         })
-// }
