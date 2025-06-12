8eec22fbc403df044ccaa0919ac7cb8ef8fbf073
Luyanda Blose <161873288+L9gc@users.noreply.github.com>
2025-06-12 16:50:51 +0200

    Update Script.js

--- 17th Project - Movie App/Script.js
+++ 17th Project - Movie App/Script.js
@@ -1,6 +1,6 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=acbc0929d4b583759fd7720f92a796e0&page=1';
+const API_URL = '#########';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=acbc0929d4b583759fd7720f92a796e0&query="';
+const SEARCH_URL = '#########';
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
